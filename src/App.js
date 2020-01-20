75b9a3935b4c287916079a13c5628ccca088ecbb
David Markel <davidgmarkel@gmail.com>
2020-01-20 18:06:52 -0500

    add new component, form, import to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './Containers/card'
+import Form from './Components/form'
 
 function App() {
   return (
     <div className="App">
       < Card />
+      <Form />
     </div>
   );
 }
