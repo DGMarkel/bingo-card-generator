dbc9dc4c8af53cfdf506d5caa55b10444a031511
David Markel <davidgmarkel@gmail.com>
2020-01-26 21:41:57 -0500

    add renderSquares() to Card

--- src/Containers/card.js
+++ src/Containers/card.js
@@ -3,10 +3,15 @@ import Square from '../Components/square'
 
 export default class Card extends Component {
 
+  renderSquares = () => {
+    const words = this.props.words.split(",");
+    return words.map( word => <div><Square word={word} /></div>)
+  }
+
   render () {
     return (
       <div>
-        <Square />
+        { this.renderSquares() }
       </div>
     )
   }
