a322d1ec170eb3cb5321a1ab2d7e99b30837a086
David Markel <davidgmarkel@gmail.com>
2020-01-28 14:03:45 -0500

    add new class to grid divs

--- src/Containers/card.js
+++ src/Containers/card.js
@@ -10,7 +10,7 @@ export default class Card extends Component {
   generateGrid = () => {
     const cells = parseInt(this.props.cells);
     let words = this.props.words.split(",");
-    return [...Array(cells).keys()].map( (cell, i) => <div className="cell">{ words[i] || ''}</div> );
+    return [...Array(cells).keys()].map( (cell, i) => <div className={`cell ${this.flexDimensions(cells)}`}>{ words[i] || ''}</div> );
   };
 
   flexDimensions = (cells) => {
