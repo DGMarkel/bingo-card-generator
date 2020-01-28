c232358aa71c85ec2946024e02e02527e42abeba
David Markel <davidgmarkel@gmail.com>
2020-01-28 14:01:28 -0500

    add new method flexDimensions

--- src/Containers/card.js
+++ src/Containers/card.js
@@ -6,11 +6,23 @@ export default class Card extends Component {
    generates bingo grid based on value of this.props.cells
    passes words to the grid based on array index
   */
+
   generateGrid = () => {
     const cells = parseInt(this.props.cells);
     let words = this.props.words.split(",");
     return [...Array(cells).keys()].map( (cell, i) => <div className="cell">{ words[i] || ''}</div> );
   };
+
+  flexDimensions = (cells) => {
+    switch(cells) {
+      case 9:
+        return "flex-thirds"
+      case 16:
+        return "flex-quarters"
+      case 25:
+        return "flex-fifths"
+    }
+  }
 
   render () {
     return (
